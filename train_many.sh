e26f80c41f9bda1a6233ea196e5070d8163bd041
xander <xandersteenbruggex@gmail.com>
2023-11-21 06:49:50 -0800

    updates to gitignore and dockerignore

--- train_many.sh
+++ train_many.sh
@@ -1,4 +1,73 @@
 cog predict \
+    -i run_name="banny" \
+    -i checkpointing_steps="500" \
+    -i debug="True" \
+    -i lora_rank="4" \
+    -i lora_training_urls="https://storage.googleapis.com/public-assets-xander/A_workbox/lora_training_sets/banny_all.zip" \
+    -i max_train_steps="1000" \
+    -i concept_mode="object" \
+    -i resolution=960 \
+    -i seed="0"
+
+cog predict \
+    -i run_name="banny_no_hard_pivot" \
+    -i checkpointing_steps="500" \
+    -i debug="True" \
+    -i lora_rank="4" \
+    -i lora_training_urls="https://storage.googleapis.com/public-assets-xander/A_workbox/lora_training_sets/banny_all.zip" \
+    -i max_train_steps="1000" \
+    -i concept_mode="object" \
+    -i resolution=960 \
+    -i hard_pivot="False" \
+    -i seed="0"
+
+cog predict \
+    -i run_name="does" \
+    -i checkpointing_steps="1000" \
+    -i debug="True" \
+    -i lora_rank="12" \
+    -i lora_training_urls="https://storage.googleapis.com/public-assets-xander/A_workbox/lora_training_sets/does.zip" \
+    -i max_train_steps="4000" \
+    -i concept_mode="style" \
+    -i resolution=960 \
+    -i seed="0"
+
+cog predict \
+    -i run_name="does_no_hard_pivot" \
+    -i checkpointing_steps="1000" \
+    -i debug="True" \
+    -i lora_rank="12" \
+    -i lora_training_urls="https://storage.googleapis.com/public-assets-xander/A_workbox/lora_training_sets/does.zip" \
+    -i max_train_steps="4000" \
+    -i concept_mode="style" \
+    -i resolution=960 \
+    -i hard_pivot="False" \
+    -i seed="0"
+
+cog predict \
+    -i run_name="kojii_all" \
+    -i checkpointing_steps="500" \
+    -i debug="True" \
+    -i lora_rank="6" \
+    -i lora_training_urls="https://storage.googleapis.com/public-assets-xander/A_workbox/lora_training_sets/koji_all.zip" \
+    -i max_train_steps="2000" \
+    -i concept_mode="object" \
+    -i resolution=960 \
+    -i seed="0"
+
+cog predict \
+    -i run_name="kojii_all_no_hard_pivot" \
+    -i checkpointing_steps="500" \
+    -i debug="True" \
+    -i lora_rank="6" \
+    -i lora_training_urls="https://storage.googleapis.com/public-assets-xander/A_workbox/lora_training_sets/koji_all.zip" \
+    -i max_train_steps="2000" \
+    -i concept_mode="object" \
+    -i resolution=960 \
+    -i hard_pivot="False" \
+    -i seed="0"
+
+cog predict \
     -i run_name="xander_5_no_hard_pivot" \
     -i checkpointing_steps="250" \
     -i debug="True" \
@@ -33,39 +102,6 @@ cog predict \
     -i max_train_steps="1000" \
     -i concept_mode="face" \
     -i left_right_flip_augmentation="False" \
-    -i resolution=960 \
-    -i seed="0"
-
-cog predict \
-    -i run_name="does" \
-    -i checkpointing_steps="1000" \
-    -i debug="True" \
-    -i lora_rank="12" \
-    -i lora_training_urls="https://storage.googleapis.com/public-assets-xander/A_workbox/lora_training_sets/does.zip" \
-    -i max_train_steps="5000" \
-    -i concept_mode="style" \
-    -i resolution=960 \
-    -i seed="0"
-
-cog predict \
-    -i run_name="banny" \
-    -i checkpointing_steps="500" \
-    -i debug="True" \
-    -i lora_rank="4" \
-    -i lora_training_urls="https://storage.googleapis.com/public-assets-xander/A_workbox/lora_training_sets/banny_all.zip" \
-    -i max_train_steps="1000" \
-    -i concept_mode="object" \
-    -i resolution=960 \
-    -i seed="0"
-
-cog predict \
-    -i run_name="kojii_all" \
-    -i checkpointing_steps="500" \
-    -i debug="True" \
-    -i lora_rank="6" \
-    -i lora_training_urls="https://storage.googleapis.com/public-assets-xander/A_workbox/lora_training_sets/koji_all.zip" \
-    -i max_train_steps="2000" \
-    -i concept_mode="object" \
     -i resolution=960 \
     -i seed="0"
 
@@ -119,14 +155,15 @@ cog predict \
     -i seed="0"
 
 cog predict \
-    -i run_name="clipx_200_object" \
+    -i run_name="clipx_200_style_no_hard" \
     -i checkpointing_steps="500" \
     -i debug="True" \
     -i lora_rank="12" \
     -i lora_training_urls="https://storage.googleapis.com/public-assets-xander/A_workbox/lora_training_sets/clipx_200.zip" \
     -i max_train_steps="3000" \
-    -i concept_mode="object" \
+    -i concept_mode="style" \
     -i resolution=960 \
+    -i hard_pivot="False" \
     -i seed="0"
 
 cog predict \
@@ -141,13 +178,14 @@ cog predict \
     -i seed="0"
 
 cog predict \
-    -i run_name="does_bs3" \
+    -i run_name="does_bs3_no_hard_pivot" \
     -i checkpointing_steps="1000" \
     -i debug="True" \
     -i lora_rank="12" \
     -i lora_training_urls="https://storage.googleapis.com/public-assets-xander/A_workbox/lora_training_sets/does.zip" \
-    -i max_train_steps="3000" \
+    -i max_train_steps="4000" \
     -i concept_mode="style" \
     -i train_batch_size=3 \
     -i resolution=960 \
+    -i hard_pivot="False" \
     -i seed="0"
